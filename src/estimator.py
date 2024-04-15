dd03386469d39f406fbaa106ef18c799802c0b5a
faridhn2 <111419840+faridhn2@users.noreply.github.com>
2024-04-15 17:11:12 +0330

    Update estimator.py

--- src/estimator.py
+++ src/estimator.py
@@ -256,7 +256,7 @@ class TfPoseEstimator:
             graph_def = tf.compat.v1.GraphDef()
             graph_def.ParseFromString(f.read())
 
-        self.graph = tf.get_default_graph()
+        self.graph = tf.compat.v1.get_default_graph()
         tf.import_graph_def(graph_def, name='TfPoseEstimator')
         self.persistent_sess = tf.Session(graph=self.graph)
 
